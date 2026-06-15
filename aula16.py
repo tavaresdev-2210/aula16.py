d9e97c693ff899ec1cec6285b6996077156e83eb
tavaresdev-2210 <lucas.t.rosa@outlook.com>
2026-06-15 17:55:51 -0300

    Add files via upload

---
+++ aula16.py
@@ -0,0 +1,22 @@
+#Introduçao aos blocos de codigos que seriam as condicionais 
+#if/ elif ..... / else
+#se / se nao se / se não
+#"sim"/ "não" / "nenhum dos dois"
+#Essas seriam traduções
+# sempre usar o TAB para pular as 4 linhas antes de digitar as condicionais  
+
+entrada = input ('Você deseja "continuar" ou "sair"? ')
+
+if entrada == 'continuar':
+    print('Você continuou na conversa')
+#neste meio um bot aqui seria a continuacao se o cliente quisesse continuar na conversa     
+    print('Selecione as opcões')
+
+elif entrada == 'sair':
+    print('Você saiu da conversa')
+else:
+    print('Você não digitou nenhuma das opções')     
+
+
+
+
